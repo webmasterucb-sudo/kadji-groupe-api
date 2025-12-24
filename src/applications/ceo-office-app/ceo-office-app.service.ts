885fa7070cc6b192c07c4e16bae7cedbdebc2671
CHRISTIAN NANA WANDA <christian.nana@sa-ucb.com>
2025-12-24 01:14:27 +0100

    Crrection bugs sur AZURE

--- src/applications/ceo-office-app/ceo-office-app.service.ts
+++ src/applications/ceo-office-app/ceo-office-app.service.ts
@@ -19,10 +19,12 @@ export class TravelTicketsService {
     const createdTicket = new this.travelTicketModel(createTravelTicketDto);
     return createdTicket.save();
   }
+  
 
   async findAll(): Promise<TravelTicket[]> {
-    return this.travelTicketModel.find().sort({updatedAt: -1 }).limit(1200).exec();
+    return this.travelTicketModel.find().sort({updatedAt: -1 }).limit(1200).exec(); 
   }
+
 
   async findOne(id: string): Promise<TravelTicket> {
     const ticket = await this.travelTicketModel.findById(id).exec();
